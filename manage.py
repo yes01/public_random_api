99bb7dbe2b0383d4ec4f344d5ec5161823628ba2
lixiang <>
2023-02-24 13:15:01 +0800

    屏蔽相关代码

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@ from app import create_app
 from flask_script import Manager
 
 app = create_app("development")
-# app.config.update(RESTFUL_JSON=dict(ensure_ascii=False))
 app.config['JSON_AS_ASCII'] = False
 
 manager = Manager(app)
